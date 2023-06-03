e180acd1f1491655e38402407fa6e2f6545bfbc7
Iya Rivvikyn <101973571+iyarivky@users.noreply.github.com>
2023-06-04 04:10:49 +0700

    mamah aku takut

--- script-foolparse.js
+++ script-foolparse.js
@@ -1,3 +1,8 @@
+async function fetchConfig(url) {
+  const response = await fetch(url);
+  return await response.json();
+}
+
 async function fetchFoolAPI(vRay) {
   let formDataAPI = new FormData();
   formDataAPI.append("urls", vRay);
@@ -21,6 +26,7 @@ async function fetchUrlAllOrigin(url) {
   return await response.text();
 }
 
+
 async function getData() {
   let akun = "https://pastebin.com/raw/hPsEFHmG"; // your mom account
   let inputData = akun.startsWith("http")
@@ -41,19 +47,38 @@ async function getData() {
   let nameProxy = outboundsConfig.map(item => item.tag);
   console.log(nameProxy);
   
-  const baseConfigUrl = "https://raw.githubusercontent.com/iyarivky/sing-ribet/main/config/config.json";
-  const baseConfigResponse = await fetch(baseConfigUrl);
-  const baseConfig = await baseConfigResponse.json();
-  baseConfig.outbounds.forEach(outbound => {
+  const urls = {
+    Base: "https://raw.githubusercontent.com/iyarivky/sing-ribet/main/config/config.json",
+    Simple: "https://raw.githubusercontent.com/iyarivky/sing-ribet/main/config/config-simple.json",
+    Bfm: "https://raw.githubusercontent.com/iyarivky/sing-ribet/main/config/config-bfm.json",
+    bfmSimple: "https://raw.githubusercontent.com/iyarivky/sing-ribet/main/config/config-bfm-simple.json"
+  };
+  const configs = {};
+  for (const [key, url] of Object.entries(urls)) {
+    configs[key] = await fetchConfig(url);
+  };
+  console.log(configs.Base)
+  
+  configs.Base.outbounds.forEach(outbound => {
     if (["Internet", "Best Latency", "Lock Region ID"].includes(outbound.tag)) {
       outbound.outbounds.push(...nameProxy);
     }
   });
-  let addProxy = baseConfig.outbounds.findIndex(outbound => outbound.tag === 'Lock Region ID');
-  baseConfig.outbounds.splice(addProxy + 1, 0, ...outboundsConfig);
+  let addProxy = configs.Base.outbounds.findIndex(outbound => outbound.tag === 'Lock Region ID');
+  configs.Base.outbounds.splice(addProxy + 1, 0, ...outboundsConfig);
 
-  let formattedBaseConfig = JSON.stringify(baseConfig, null, 2);
+
+  configs.Simple.outbounds.forEach(outbound => {
+    if (["Internet", "Best Latency"].includes(outbound.tag)) {
+      outbound.outbounds.push(...nameProxy);
+    }
+  });
+  let addProxySimple = configs.Simple.outbounds.findIndex(outbound => outbound.tag === 'Best Latency');
+  configs.Simple.outbounds.splice(addProxySimple + 1, 0, ...outboundsConfig);
+
+  let formattedBaseConfig = JSON.stringify(configs.Base, null, 2);
+  let formattedSimpleConfig = JSON.stringify(configs.Simple, null, 2);
   console.log(formattedBaseConfig)
-
+  console.log(formattedSimpleConfig)
 }
 getData();
